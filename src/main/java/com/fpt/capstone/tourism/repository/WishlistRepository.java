ca333f39971bfc7a9f6599fedfea201aca471ebf
LanThan99 <lananhthan99@gmail.com>
2025-03-29 11:23:41 +0700

    add: customer wishlist

---
+++ src/main/java/com/fpt/capstone/tourism/repository/WishlistRepository.java
@@ -0,0 +1,12 @@
+package com.fpt.capstone.tourism.repository;
+
+import com.fpt.capstone.tourism.model.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+    List<Wishlist> findByUserId(Long id);
+
+    Wishlist findByItemId(Long itemId);
+}
