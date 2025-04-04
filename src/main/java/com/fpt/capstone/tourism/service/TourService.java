9ce40312dd5bee4aa5812bb6fecd02b961e61b09
LanThan99 <lananhthan99@gmail.com>
2025-04-04 21:18:33 +0700

    modify dashboard

--- src/main/java/com/fpt/capstone/tourism/service/TourService.java
+++ src/main/java/com/fpt/capstone/tourism/service/TourService.java
@@ -48,5 +48,5 @@ public interface TourService {
 
     GeneralResponse<?> rejectTourProcess(Long tourId);
 
-    GeneralResponse<?> viewDashboard(LocalDate toDate);
+    GeneralResponse<?> viewDashboard(LocalDate fromDate,LocalDate toDate);
 }
