9fce63b35276784e01142010c3bd5e999f968ebc
LanThan99 <lananhthan99@gmail.com>
2025-05-06 17:24:58 +0700

    update

--- src/main/java/com/fpt/capstone/tourism/dto/common/CostAccountDTO.java
+++ src/main/java/com/fpt/capstone/tourism/dto/common/CostAccountDTO.java
@@ -3,6 +3,7 @@ package com.fpt.capstone.tourism.dto.common;
 import com.fpt.capstone.tourism.model.Transaction;
 import com.fpt.capstone.tourism.model.enums.CostAccountStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +16,12 @@ import lombok.NoArgsConstructor;
 public class CostAccountDTO {
     private Long id;
     private String content;
+    @Min(value = 1, message = "Đơn giá phải dương")
     private Double amount; // Đơn giá
     private int discount;
+    @Min(value = 1, message = "Số lượng phải dương")
     private int quantity;
+    @Min(value = 1, message = "Số tiền phải dương")
     private Double finalAmount;
     private CostAccountStatus status;
 }
